4250470831518011391cb01d99e008bbaf61eadb
Glory <dmb07223@jinhakapply.com>
2024-12-23 14:42:27 +0900

    added app/index view

--- django_for_beginners/mysite/app/views.py
+++ django_for_beginners/mysite/app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+from app.models import Post
 
 # Create your views here.
+def index(request: HttpRequest) -> HttpRequest:
+    qs = Post.objects.all()
+
+    return render(request, "index.html", {"post_list": qs})
